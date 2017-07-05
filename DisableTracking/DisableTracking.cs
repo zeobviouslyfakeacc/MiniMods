6f38360e52cede016cb6f01edc944f57bbce9e0d
zeobviouslyfakeacc <zeobviouslyfakeacc@users.noreply.github.com>
2017-07-05 20:39:25 +0200

    Include DisableTracking mod

---
+++ DisableTracking/DisableTracking.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+public class DisableTracking
+{
+	public static void OnLoad()
+	{
+		GA_Queue.EndSubmit();
+		FieldInfo baseURLField = typeof(GA_Submit).GetField("_baseURL", BindingFlags.NonPublic | BindingFlags.Static);
+		baseURLField.SetValue(null, "://localhost");
+		Debug.Log("[DisableTracking] Redirecting all tracking events to localhost");
+
+		Analytics.enabled = false;
+		Analytics.deviceStatsEnabled = false;
+		Analytics.limitUserTracking = true;
+		Debug.Log("[DisableTracking] Attempted to limit Unity tracking");
+	}
+}
---
+++ DisableTracking/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DisableTracking")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DisableTracking")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("425f5c84-f38c-4ee4-a769-968b30d072da")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
