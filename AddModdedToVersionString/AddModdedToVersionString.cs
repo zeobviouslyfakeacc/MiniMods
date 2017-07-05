318eb438346d06b0ec8305499a72b000258ce82a
zeobviouslyfakeacc <zeobviouslyfakeacc@users.noreply.github.com>
2017-07-05 20:27:57 +0200

    Add AddModdedToVersionString default mod

---
+++ AddModdedToVersionString/AddModdedToVersionString.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+class AddModdedToVersionString
+{
+	public static void OnLoad()
+	{
+		GameManager.m_GameVersionString = "Modded " + GameManager.m_GameVersionString;
+		Debug.Log(" === This game is MODDED. Do not report any issues to Hinterland! === ");
+	}
+}
---
+++ AddModdedToVersionString/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AddModdedToVersionString")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AddModdedToVersionString")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("743b0742-a358-4902-b7b7-322bd8781709")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
