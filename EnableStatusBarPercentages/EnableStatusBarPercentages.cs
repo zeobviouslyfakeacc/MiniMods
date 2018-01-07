ed1fed1bbb91176ad5075bb6e57f8cab79eb8cb2
zeobviouslyfakeacc <zeobviouslyfakeacc@users.noreply.github.com>
2018-01-07 01:30:10 +0100

    Add EnableStatusBarPercentages: Show hidden labels in first aid screen

---
+++ EnableStatusBarPercentages/EnableStatusBarPercentages.cs
@@ -0,0 +1,21 @@
+﻿using Harmony;
+using UnityEngine;
+
+[HarmonyPatch(typeof(Panel_FirstAid), "Start")]
+public class EnableStatusBarPercentages {
+	private static void Postfix(Panel_FirstAid __instance) {
+		ActivateAndMove(__instance.m_ColdPercentLabel);
+		ActivateAndMove(__instance.m_FatiguePercentLabel);
+		ActivateAndMove(__instance.m_ThirstPercentLabel);
+		ActivateAndMove(__instance.m_HungerPercentLabel);
+	}
+
+	private static void ActivateAndMove(UILabel label) {
+		label.pivot = UIWidget.Pivot.Center;
+		label.gameObject.transform.localPosition = new Vector3(-79, -16, 0);
+		label.fontSize = 14;
+
+		label.enabled = true;
+		NGUITools.SetActive(label.gameObject, true);
+	}
+}
---
+++ EnableStatusBarPercentages/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EnableStatusBarPercentages")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EnableStatusBarPercentages")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("876816f7-5100-496a-afe0-a46a65606481")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
