bc4eb4c9803945d4221b79233d5cb31c7b3b2b08
zeobviouslyfakeacc <zeobviouslyfakeacc@users.noreply.github.com>
2018-02-23 01:16:07 +0100

    DisableTracking: Disable sending of exception reports to Hinterland

--- DisableTracking/DisableTracking.cs
+++ DisableTracking/DisableTracking.cs
@@ -1,19 +1,38 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using UnityEngine;
 using UnityEngine.Analytics;
+using UnityEngine.CrashReportHandler;
+using UnityEngine.Networking.PlayerConnection;
 
-public class DisableTracking
-{
-	public static void OnLoad()
-	{
+public static class DisableTracking {
+
+	public static void OnLoad() {
 		GA_Queue.EndSubmit();
 		FieldInfo baseURLField = typeof(GA_Submit).GetField("_baseURL", BindingFlags.NonPublic | BindingFlags.Static);
 		baseURLField.SetValue(null, "://localhost");
-		Debug.Log("[DisableTracking] Redirecting all tracking events to localhost");
 
 		Analytics.enabled = false;
 		Analytics.deviceStatsEnabled = false;
 		Analytics.limitUserTracking = true;
-		Debug.Log("[DisableTracking] Attempted to limit Unity tracking");
+		CrashReportHandler.enableCaptureExceptions = false;
+
+		Assembly unityConnectModule = typeof(RemoteSettings).Assembly;
+		SetInternalProperty(unityConnectModule, "UnityEngine.Connect.UnityConnectSettings", "enabled", false);
+		SetInternalProperty(unityConnectModule, "UnityEngine.Advertisements.UnityAdsSettings", "enabled", false);
+
+		PlayerConnection.instance.DisconnectAll();
+		Debug.Log("[DisableTracking] Disabled GameAnalytics and limited UnityEngine tracking");
+	}
+
+	private static void SetInternalProperty(Assembly assembly, string className, string propertyName, object value) {
+		try {
+			Type type = assembly.GetType(className, true);
+			PropertyInfo prop = type.GetProperty(propertyName, BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+			prop.SetValue(null, value, null);
+		} catch (Exception ex) {
+			Debug.LogError("[DisableTracking] Could not set property '" + propertyName + "' of class '" + className + "'");
+			Debug.LogException(ex);
+		}
 	}
 }
