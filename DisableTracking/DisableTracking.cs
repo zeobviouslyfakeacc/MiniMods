0f417e6f64da44ecc3b3e7e4dea14961c926a01b
zeobviouslyfakeacc <zeobviouslyfakeacc@users.noreply.github.com>
2018-03-21 20:56:35 +0100

    DisableTracking: Prepare for the next iteration of "Spying on The Players"

--- DisableTracking/DisableTracking.cs
+++ DisableTracking/DisableTracking.cs
@@ -1,38 +1,33 @@
 ﻿using System;
-using System.Reflection;
-using UnityEngine;
-using UnityEngine.Analytics;
-using UnityEngine.CrashReportHandler;
-using UnityEngine.Networking.PlayerConnection;
+using Harmony;
 
 public static class DisableTracking {
 
-	public static void OnLoad() {
-		GA_Queue.EndSubmit();
-		FieldInfo baseURLField = typeof(GA_Submit).GetField("_baseURL", BindingFlags.NonPublic | BindingFlags.Static);
-		baseURLField.SetValue(null, "://localhost");
-
-		Analytics.enabled = false;
-		Analytics.deviceStatsEnabled = false;
-		Analytics.limitUserTracking = true;
-		CrashReportHandler.enableCaptureExceptions = false;
-
-		Assembly unityConnectModule = typeof(RemoteSettings).Assembly;
-		SetInternalProperty(unityConnectModule, "UnityEngine.Connect.UnityConnectSettings", "enabled", false);
-		SetInternalProperty(unityConnectModule, "UnityEngine.Advertisements.UnityAdsSettings", "enabled", false);
-
-		PlayerConnection.instance.DisconnectAll();
-		Debug.Log("[DisableTracking] Disabled GameAnalytics and limited UnityEngine tracking");
+	[HarmonyPatch(typeof(Utils), "SendAnalyticsEvent", new Type[] { typeof(string), typeof(int) })]
+	private static class DisableAnalytics1 {
+		private static bool Prefix() {
+			return false;
+		}
 	}
 
-	private static void SetInternalProperty(Assembly assembly, string className, string propertyName, object value) {
-		try {
-			Type type = assembly.GetType(className, true);
-			PropertyInfo prop = type.GetProperty(propertyName, BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
-			prop.SetValue(null, value, null);
-		} catch (Exception ex) {
-			Debug.LogError("[DisableTracking] Could not set property '" + propertyName + "' of class '" + className + "'");
-			Debug.LogException(ex);
+	[HarmonyPatch(typeof(Utils), "SendAnalyticsEvent", new Type[] { typeof(string), typeof(string) })]
+	private static class DisableAnalytics2 {
+		private static bool Prefix() {
+			return false;
+		}
+	}
+
+	[HarmonyPatch(typeof(Utils), "SendAnalyticsEvent", new Type[] { typeof(string), typeof(string), typeof(string) })]
+	private static class DisableAnalytics3 {
+		private static bool Prefix() {
+			return false;
+		}
+	}
+
+	[HarmonyPatch(typeof(Utils), "SendAnalyticsBucketEvent", new Type[] { typeof(string), typeof(float), typeof(int[]) })]
+	private static class DisableAnalytics4 {
+		private static bool Prefix() {
+			return false;
 		}
 	}
 }
